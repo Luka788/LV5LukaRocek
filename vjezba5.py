7c7614f4eea34d346606798e2c04ae9e73bc796f
Roček Luka <1311033724@FPZ.HR>
2024-11-05 10:59:46 +0100

    Dodavanje prve verzije koda

---
+++ vjezba5.py
@@ -0,0 +1,11 @@
+def provjer_broja(broj):
+    if 10 <= broj <= 100:
+        return f"Broj {broj} je unutar raspona."
+    else:
+        return f"Broj {broj} je izvan raspona."
+if __name__ == "__main__":
+    try:
+        unesi_broj = int(input("Unesite broj: "))
+        print(provjer_broja(unesite_broj))
+    except ValueError:
+        print("Unesena vrijednost nije broj.")
